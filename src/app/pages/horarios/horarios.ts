925a82f4bde0fe51e5d83617c85f8905b9418d3a
h3m5t <henriquee.trajanoo@gmail.com>
2025-11-27 15:08:31 -0300

    esqueleto feito

--- src/app/pages/horarios/horarios.ts
+++ src/app/pages/horarios/horarios.ts
@@ -7,5 +7,8 @@ import { Component } from '@angular/core';
   styleUrl: './horarios.css',
 })
 export class Horarios {
-
+horarios = [
+    { id: 1, nome: 'Henrique', desc: 'Segunda a Sexta: 08:00 - 17:00' },
+    { id: 2, nome: 'Erick', desc: 'Terça a Sábado: 13:00 - 22:00' }
+  ];
 }
